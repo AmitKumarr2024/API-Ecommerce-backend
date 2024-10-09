966930e267051abc50c61e7ff41b334b2043af14
Amit kumar <codes.amitkumar@gmail.com>
2024-10-10 00:25:30 +0530

    four commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Import the CORS package
 import connectToMongoDB from "./Config/mongodb.js";
 import swaggerUiExpress from "swagger-ui-express";
 import apiDocs from "./swagger.json" assert { type: "json" };
@@ -9,24 +10,26 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-const app = express(); // Removed `new`
+const app = express(); 
 const PORT = process.env.PORT || 7010;
 
+// Configure CORS options if needed
+const corsOptions = {
+  origin: "*", // Allows all origins, you can specify your frontend URL here
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions)); // Use the CORS middleware
 app.use(express.json());
-// Use cookie-parser middleware
-app.use(cookieParser());
+app.use(cookieParser()); // Use cookie-parser middleware
 
 // Setup Swagger UI for API documentation
-// http://localhost:7002/api-docs/#/    enter
 app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(apiDocs));
 
+// Open access to user and product routes
 app.use("/api/user/", UserRoute);
-// product
 app.use("/api/product/", ProductRoute);
-
-// app.get("/", (req, res) => {
-//   res.status(200).send("<h1>Hello Amit</h1>");
-// });
 
 app.listen(PORT, () => {
   console.log(`Server started successfully on port ${PORT}`);
